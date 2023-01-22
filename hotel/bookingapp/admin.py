5a2a75e05ffdb4c7df5ce6dad7a32e5779710817
mumop <mumopeter17@gmail.com>
2023-01-22 11:03:06 +0300

    Adds Guest model

--- hotel/bookingapp/admin.py
+++ hotel/bookingapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Room, Booking
+from .models import Room, Booking, Guest
 
 # Registration of models
 admin.site.register(Room)
 admin.site.register(Booking)
+admin.site.register(Guest)
--- hotel/bookingapp/models.py
+++ hotel/bookingapp/models.py
@@ -2,6 +2,21 @@ from django .db import models
 from django.conf import settings
 # Create models
 
+class Guest(models.Model):
+    GENDER = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+        ('P', 'Prefer Not Say'),
+    )
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                on_delete=models.CASCADE)
+    gender = models.CharField(max_length=10, choices=GENDER)
+    dob = models.DateField(name='Date of Birth')
+    phone_no = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.user}'
 
 class Room (models.Model):
     ROOM_CATEGORIES = (
@@ -16,19 +31,18 @@ class Room (models.Model):
     beds = models.IntegerField()
     capacity = models.IntegerField()
 
-# function
+    # function
 
-
-def _str_(self):
-    return f'{self.number}.{self.category} with {self.beds} beds for {self.capacity} people'
+    def __str__(self):
+        return f'{self.number} -> {self.category} with {self.beds} beds for {self.capacity} people'
 
 
 class Booking(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+    user = models.ForeignKey(Guest,
                              on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     check_in = models.DateTimeField()
     check_out = models.DateTimeField()
 
-    def _str_(self):
+    def __str__(self):
         return f'{self.user} has booked {self.room} from {self.check_in} to {self.check_out}'
